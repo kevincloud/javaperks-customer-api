8e00e9bc70ea348bc008a6a1cf93bc87ea0a8acf
Kevin Cochran <kcochran@hashicorp.com>
2020-04-23 18:31:49 -0400

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,5 +5,5 @@ export VAULT_TOKEN=$(curl --request POST \
   --data "{\"jwt\": \"$KUBE_TOKEN\", \"role\": \"cust-api\"}" \
   $VAULT_ADDR/v1/auth/kubernetes/login | jq -r .auth.client_token) && echo $VAULT_TOKEN
 
-echo "vaultAddress: $VAULT_ADDR >>/app/config.yml"
-echo "vaultToken: $VAULT_TOKEN >>/app/config.yml"
+echo "vaultAddress: $VAULT_ADDR" >>/app/config.yml
+echo "vaultToken: $VAULT_TOKEN" >>/app/config.yml
